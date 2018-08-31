c92fd578fe6806ca1fdc2c62dce62720f1488fe0
IramML <IramMartinezLamas@gmail.com>
2018-08-30 21:24:51 -0500

    Renovate web page

--- js/navigationToggle.js
+++ js/navigationToggle.js
@@ -1,15 +1,16 @@
-var navigation;
-var changeTheme;
-var githubImg;
-var twitterImg;
+var openNav=document.getElementById("open-nav");
+var navigation=document.getElementById("navigation");
 
+var navOpen=false;
+var site=0;
 
-function initialize(site){
-	navigation=document.getElementById("navigation");
-	changeTheme=document.getElementById("change-theme");
-	githubImg=document.getElementById("github-network");
-	twitterImg=document.getElementById("twitter-network");
+openNav.onclick=function(){toggle()}
 
-	changeTheme.onclick=function(){switchTheme()};
-	initializeTheme(site);
+function toggle(){
+	if (navOpen) {
+		navigation.style.height="0px";
+	}else{
+		navigation.style.height="auto";
+	}
+	navOpen=!navOpen
 }
