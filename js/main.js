25637c5681412979232fb87f8cfeb10fc2891dd7
IramML <IramMartinezLamas@gmail.com>
2018-12-27 23:52:20 -0600

    New interface

---
+++ js/main.js
@@ -0,0 +1,21 @@
+btnScroll=document.getElementById('scroll-projects');
+projects=document.getElementsByClassName('project');
+descriptions=document.getElementsByClassName('description');
+impleneOnClicks();
+
+function impleneOnClicks() {
+    btnScroll.onclick = function () {
+        document.getElementById('projects-content').scrollIntoView();
+    };
+    for (var i = 0; i < projects.length; i++) {
+        showDescription(i);
+    }
+}
+function showDescription(index){
+    projects[index].onclick=function () {
+        if(descriptions[index].style.height!=="auto")
+            descriptions[index].style.height="auto";
+        else
+            descriptions[index].style.height="0";
+    };
+}
