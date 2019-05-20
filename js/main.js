80a6bc7fdc1b8332e4a4c68cb3247a8f20653440
IramML <IramMartinezLamas@gmail.com>
2019-05-19 19:51:21 -0500

    New interface, again

--- js/main.js
+++ js/main.js
@@ -1,15 +1,42 @@
+AOS.init();
 const profesion = baffle(".profesion");
 
 profesion.set({
     characters: '█▓▓ ░░>██ ▓█▓>▓ ▓<█ ░<▒░▓ █░<█ █▒> ▓░▓< ▒▓░░',
-    speed: 90
+    speed: 75
 });
 
 profesion.start();
 
-profesion.reveal(3000);
-console.log("tagline");
-$('#tagline').t({
+
+var interval=setInterval(function(){
+  clearInterval(interval);
+  profesion.reveal(1000);
+},1000);
+
+// Add smooth scrolling to all links
+$("a").on('click', function(event) {
+	
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  $('#tagline').t({
     beep: false,
     caret:'<span style="color:red;">|</span>',
     typing:function(elm,chr){
@@ -21,3 +48,33 @@ setTimeout(function(){
     $('.loader').hide();
 
 }, 1800);
+$(document).ready(function () {
+  //  isotope
+  $('#projects').waitForImages(function () {
+      var $container = $('.portfolio-container');
+      $container.isotope({
+          filter: '*',
+      });
+
+      $('.portfolio-filter a').click(function () {
+          $('.portfolio-filter .active').removeClass('active');
+          $(this).addClass('active');
+
+          var selector = $(this).attr('data-filter');
+          $container.isotope({
+              filter: selector,
+              animationOptions: {
+                  duration: 500,
+                  animationEngine: "jquery"
+              }
+          });
+          return false;
+      });
+
+  });
+
+  //animatedModal
+  $("#demo01").animatedModal();
+  $("#demo02").animatedModal2();
+  $("#demo03").animatedModal3();
+});
